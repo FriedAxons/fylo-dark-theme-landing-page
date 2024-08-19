da83306d9539693c70051ccf96de840a3fcfc32a
FriedAxons <friedaxons@proton.me>
2024-08-18 20:42:45 -0500

    Styles applied to testimonials section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,16 +1,18 @@
 const Testimonials = () => {
   return (
     <div className="testimonials-section">
-      <div className="testimonial">
+      <div className="testimonial first-testimonial">
         <p>
           Fylo has improved our team productivity by an order of magnitude.
           Since making the switch our team has become a well-oiled collaboration
           machine.
         </p>
         <div className="profile-section">
-          <div className="profile-picture">
-            <img src="../../images/profile-1.jpg" alt="Profile picture" />
-          </div>
+          <img
+            src="../../images/profile-1.jpg"
+            alt="Profile picture"
+            className="profile-picture"
+          />
           <div className="profile-name-and-bio-container">
             <h5 className="profile-name">Satish Patel</h5>
             <h6 className="company-bio">Founder & CEO, Huddle</h6>
@@ -25,9 +27,11 @@ const Testimonials = () => {
           machine.
         </p>
         <div className="profile-section">
-          <div className="profile-picture">
-            <img src="../../images/profile-2.jpg" alt="Profile picture" />
-          </div>
+          <img
+            src="../../images/profile-2.jpg"
+            alt="Profile picture"
+            className="profile-picture"
+          />
           <div className="profile-name-and-bio-container">
             <h5 className="profile-name">Bruce McKenzie</h5>
             <h6 className="company-bio">Founder & CEO, Huddle</h6>
@@ -35,16 +39,18 @@ const Testimonials = () => {
         </div>
       </div>
 
-      <div className="testimonial">
+      <div className="testimonial last-testimonial">
         <p>
           Fylo has improved our team productivity by an order of magnitude.
           Since making the switch our team has become a well-oiled collaboration
           machine.
         </p>
         <div className="profile-section">
-          <div className="profile-picture">
-            <img src="../../images/profile-3.jpg" alt="Profile picture" />
-          </div>
+          <img
+            src="../../images/profile-3.jpg"
+            alt="Profile picture"
+            className="profile-picture"
+          />
           <div className="profile-name-and-bio-container">
             <h5 className="profile-name">Iva Boyd</h5>
             <h6 className="company-bio">Founder & CEO, Huddle</h6>
