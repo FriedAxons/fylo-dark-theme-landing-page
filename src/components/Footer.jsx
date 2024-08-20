0ddeaf4cebf19bf908e76f6b139439fb707e4768
FriedAxons <friedaxons@proton.me>
2024-08-20 13:24:16 -0500

    Added icons and styles for footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,9 +9,34 @@ const Footer = () => {
           eiusmod tempor incididunt ut labore et dolore magna aliqua
         </p>
       </div>
-      <div className="contact-info-section"></div>
-      <div className="about-and-contact-us-section"></div>
-      <div className="social-links-section"></div>
+      <div className="contact-info-section">
+        <div className="phone-number-container">
+          <i className="fa-solid fa-phone-volume"></i>
+          <p>+1-543-123-4567</p>
+        </div>
+        <div className="email-address-container">
+          <i className="fa-solid fa-envelope"></i>
+          <p>example@fylo.com</p>
+        </div>
+      </div>
+      <div className="about-and-contact-us-section">
+        <div className="about-section">
+          <p>About Us</p>
+          <p>Jobs</p>
+          <p>Press</p>
+          <p>Blog</p>
+        </div>
+        <div className="contact-section">
+          <p>Contact Us</p>
+          <p>Terms</p>
+          <p>Privacy</p>
+        </div>
+      </div>
+      <div className="social-links-section">
+        <i className="fa-brands fa-facebook-f"></i>
+        <i className="fa-brands fa-twitter"></i>
+        <i className="fa-brands fa-instagram"></i>
+      </div>
     </footer>
   );
 };
